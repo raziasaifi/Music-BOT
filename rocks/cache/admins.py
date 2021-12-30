be9d0bcbc090a92beec82a6d472b5f1b24f9f8d0
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-12-30 17:38:40 +0500

    Telegram Music Bot

---
+++ rocks/cache/admins.py
@@ -0,0 +1,23 @@
+# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+# Without Credit (Mother Fucker)
+# Rocks © @Dr_Asad_Ali © Rocks
+# Copyright (C) 2021 By @Itz_VeNom_xD 
+# Copyright (C) 2021 By @Dr_Asad_Ali
+# Copyright (C) 2021 By @HarshitSharma361
+
+
+from typing import List, Dict, Union
+
+
+admins: Dict[int, List[int]] = {}
+
+
+def set(chat_id: int, admins_: List[int]):
+    admins[chat_id] = admins_
+
+
+def get(chat_id: int) -> Union[List[int], bool]:
+    if chat_id in admins:
+        return admins[chat_id]
+
+    return False
