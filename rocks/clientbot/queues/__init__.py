226cd427122fe475fa6ffa4b03a8b0c09a766c4d
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-12-30 17:40:17 +0500

    Telegram Music Bot

--- rocks/clientbot/queues/__init__.py
+++ rocks/clientbot/queues/__init__.py
@@ -1,1 +1,9 @@
+# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+# Without Credit (Mother Fucker)
+# Rocks © @Dr_Asad_Ali © Rocks
+# Copyright (C) 2021 By @Itz_VeNom_xD 
+# Copyright (C) 2021 By @Dr_Asad_Ali
+# Copyright (C) 2021 By @HarshitSharma361
 
+
+from .queues import put, get, is_empty, task_done, clear
